4edb28c9f48d275fe9f54064acc6f207ba1dd044
Thanida Jongarnon <bameejrn@outlook.com>
2021-05-17 07:54:54 +0700

    Add signin and register nav

--- src/components/screens/nav.js
+++ src/components/screens/nav.js
@@ -1,8 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import NavbarBootstrap from 'react-bootstrap/Navbar'
 import NavBootstrap from 'react-bootstrap/Nav'
+import { Button } from 'react-bootstrap';
 import logo from '../../assets/img/review.png'
 import {Link} from 'react-router-dom'
+import '../../styles/Home.css'
 
 const Navbar = () => {
     return (
@@ -22,7 +24,17 @@ const Navbar = () => {
       <Link to="/booking">Booking</Link>
       <Link to="/hostel">Hostel</Link>
     </NavBootstrap>
+    <div className="navbar-nav ms-lg-4">
+        <Link className="nav-item nav-link" to="/signin">Sign in</Link>
+      </div>
+      <div class="d-flex align-items-lg-center mt-3 mt-lg-0">
+        <Link to="/register" class="btn btn-sm btn-primary w-full w-lg-auto">
+          Register
+        </Link>
+      </div>
   </NavbarBootstrap.Collapse>
+
+  
 </NavbarBootstrap>
 
     )
