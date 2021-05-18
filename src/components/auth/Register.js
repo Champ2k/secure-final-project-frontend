6c0bbc456a3801c012f40e94b8e635c806dc3303
Winternight9 <supkakorn.tang@ku.th>
2021-05-18 16:09:00 +0700

    add css to register and add boarder to login page

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import '../../styles/auth/Register.css';
 
 
 async function registerUser(data){
@@ -36,26 +37,36 @@ export default function Register(){
     <div className="register-wrapper">
       <h1>Register</h1>
       <form onSubmit={handleSubmit} >
-        <label>
+        <div>
+        <label className="regis">
           <p>Username</p>
           <input type="text" onChange={e => setUserName(e.target.value)} />
         </label>
-        <label>
+        </div>
+        <div>
+        <label className="regis">
           <p>Password</p>
           <input type="password" onChange={e => setPassword(e.target.value)} />
         </label>
-        <label>
+        </div>
+        <div>
+        <label className="regis">
           <p>Email</p>
           <input type="text" onChange={e => setEmail(e.target.value)} />
         </label>
-        <label>
+        </div>
+        <div>
+        <label className="regis">
           <p>Firstname</p>
           <input type="text" onChange={e => setFirstName(e.target.value)} />
         </label>
-        <label>
+        </div>
+        <div>
+        <label className="regis">
           <p>Lastname</p>
           <input type="text" onChange={e => setLastName(e.target.value)} />
         </label>
+        </div>
         <div>
           <button type="submit">Submit</button>
         </div>
