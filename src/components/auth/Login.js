8e3b5f330fb8388ea32985d2c88dce7e5d0496a1
Anant Arayanant <minecraftraguna@gmail.com>
2021-05-18 18:38:57 +0700

    fix login css and js

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -33,16 +33,16 @@ export default function Login({ setToken }) {
     <div className="login-wrapper">
       <h1>Please Log In</h1>
       <form onSubmit={handleSubmit} >
-        <label>
+        <label class="login-label">
           <p>Username</p>
           <input type="text" onChange={e => setUserName(e.target.value)} />
         </label>
-        <label>
+        <label class="login-label">
           <p>Password</p>
           <input type="password" onChange={e => setPassword(e.target.value)} />
         </label>
         <div>
-          <button type="submit">Submit</button>
+          <button class="login-button" type="submit">Submit</button>
         </div>
       </form>
     </div>
