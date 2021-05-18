44b6c1d8d4e99463a30467bd02447eacddfab896
Anant Arayanant <minecraftraguna@gmail.com>
2021-05-18 19:24:11 +0700

    css regis

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -37,38 +37,39 @@ export default function Register(){
     <div className="register-wrapper">
       <h1>Register</h1>
       <form onSubmit={handleSubmit} >
-        <div>
+        <div className="regis-div">
         <label className="regis">
           <p>Username</p>
           <input type="text" onChange={e => setUserName(e.target.value)} />
         </label>
         </div>
-        <div>
+        <div className="regis-div">
         <label className="regis">
           <p>Password</p>
+          
           <input type="password" onChange={e => setPassword(e.target.value)} />
         </label>
         </div>
-        <div>
+        <div className="regis-div">
         <label className="regis">
           <p>Email</p>
           <input type="text" onChange={e => setEmail(e.target.value)} />
         </label>
         </div>
-        <div>
+        <div className="regis-div">
         <label className="regis">
           <p>Firstname</p>
           <input type="text" onChange={e => setFirstName(e.target.value)} />
         </label>
         </div>
-        <div>
+        <div className="regis-div">
         <label className="regis">
           <p>Lastname</p>
           <input type="text" onChange={e => setLastName(e.target.value)} />
         </label>
         </div>
         <div>
-          <button type="submit">Submit</button>
+          <button className="regis-button" type="submit">Submit</button>
         </div>
       </form>
     </div>
